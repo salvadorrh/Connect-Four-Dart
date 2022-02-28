4ee64d0e49e663141195a95e93a777dd22ce9ed2
salvadorrh <89811822+salvadorrh@users.noreply.github.com>
2022-02-28 02:08:30 -0700

    Main function for Connect Four
    Calling Controller class. That way using the MVC design pattern

---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'dart:convert';
+
+import 'package:ConnectFour/ConnectFour.dart' as ConnectFour;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:io';
+import 'controller.dart';
+
+main() {
+  Controller().start();
+}
